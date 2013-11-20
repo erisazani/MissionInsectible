06e1750684e20b92a1f518a1e10e025fcaff2ada
Febriana Misdianti <febriana.misdianti@gmail.com>
2013-11-20 12:24:52 +0700

    update camera layer

--- src/com/inspector/missioninsectible/scene/PlayScene.java
+++ src/com/inspector/missioninsectible/scene/PlayScene.java
@@ -1,7 +1,49 @@
 package com.inspector.missioninsectible.scene;
 
+import java.io.IOException;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.modifier.MoveModifier;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.augmentedreality.BaseAugmentedRealityGameActivity;
+import org.andengine.input.sensor.orientation.IOrientationListener;
+import org.andengine.input.sensor.orientation.OrientationData;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TextureRegion;
+
+import org.andengine.util.debug.Debug;
+
+import com.inspector.missioninsectible.MainGameActivity;
+
+//import com.example.tutor.BaseActivity;
+
+import android.util.Log;
+import android.widget.Toast;
 
 public class PlayScene extends Scene {
+	
+	MainGameActivity activity;
+	
+	public PlayScene() {
+		super();
+		activity = MainGameActivity.getSharedInstance();
+		
+		setBackground(new Background(0.0f, 0.0f, 0.0f, 0.0f));
 
+		final float centerX = (activity.mCamera.getWidth() - activity.mFaceTextureRegion.getWidth()) / 2;
+		final float centerY = (activity.mCamera.getHeight() - activity.mFaceTextureRegion.getHeight()) / 2;
+		final Sprite face = new Sprite(centerX, centerY, activity.mFaceTextureRegion, activity.getVertexBufferObjectManager());
+//		face.registerEntityModifier(new MoveModifier(30, 0, 320 - face.getWidth(), 0, 240 - face.getHeight()));
+		attachChild(face);
+	}
+	
 }
